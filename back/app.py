09fff180120f8a59cb549dd8c336375cb018c769
quinoth <dima.rudak17@gmail.com>
2025-05-11 01:23:28 +0800

    Проверка бекэнда

--- back/app.py
+++ back/app.py
@@ -1,4 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
 from config import SECRET_KEY
+app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
+
+@app.route("/")
+def home():
+    return "Hello world"
+
+if __name__ == "__main__":
+    app.run(debug=True)
