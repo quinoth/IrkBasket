ac1471a629090fdd60ec6da703ab996d618c6a05
quinoth <dima.rudak17@gmail.com>
2025-05-11 01:28:01 +0800

    Проверка полей регистрации

--- back/app.py
+++ back/app.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, jsonify
-from config import SECRET_KEY
+import bcrypt
+from db import get_db_connection
 app = Flask(__name__)
+from config import SECRET_KEY
 app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/register', methods=['POST'])
 def register():
-    pass
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    email = data.get('email')
+    role = data.get('role')
+
+    if not all([username, password, email, role]):
+        return jsonify({'message': 'Missing fields'}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
