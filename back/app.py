e1712ccff597def659750ccc67b722b50c06d9f3
quinoth <dima.rudak17@gmail.com>
2025-05-11 01:26:47 +0800

    Добавление маршрута для регистрации

--- back/app.py
+++ back/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from config import SECRET_KEY
 app = Flask(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-@app.route("/")
-def home():
-    return "Hello world"
+@app.route('/register', methods=['POST'])
+def register():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
