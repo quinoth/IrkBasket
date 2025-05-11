e5d9ab96b0a4269edf33a9fb230f09e110ca5824
quinoth <dima.rudak17@gmail.com>
2025-05-11 16:56:32 +0800

    Исправление полей в RegistrationForm

--- frontend/src/components/RegistrationForm.js
+++ frontend/src/components/RegistrationForm.js
@@ -5,7 +5,30 @@ const RegistrationForm = () => {
         <div>
             <h2>Регистрация</h2>
             <form>
-                <input type="text" placeholder="Имя пользователя" />
+                <label>
+                    Имя:
+                    <input type="text" name="first_name" />
+                </label>
+                <label>
+                    Фамилия:
+                    <input type="text" name="last_name" />
+                </label>
+                <label>
+                    Email:
+                    <input type="email" name="email" />
+                </label>
+                <label>
+                    Пароль:
+                    <input type="password" name="password" />
+                </label>
+                <label>
+                    Роль:
+                    <select name="role">
+                        <option value="player">Игрок</option>
+                        <option value="trainer">Тренер</option>
+                    </select>
+                </label>
+                <button type="submit">Зарегистрироваться</button>
             </form>
         </div>
     );
