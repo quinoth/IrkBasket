b0a4f482da5d47bab5b68ec873c16533c55e9add
quinoth <dima.rudak17@gmail.com>
2025-05-11 17:35:49 +0800

    Добавление маршрута к Profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegistrationForm from './components/RegistrationForm';
 import LoginForm from './components/LoginForm';
+import Profile from './components/Profile';
 import { AuthProvider } from './context/AuthContext';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
                 <Routes>
                     <Route path="/register" element={<RegistrationForm />} />
                     <Route path="/login" element={<LoginForm />} />
+					<Route path="/profile" element={<Profile />} />
                     <Route path="/" element={<div className="text-center mt-10">Главная страница</div>} />
                 </Routes>
             </Router>
