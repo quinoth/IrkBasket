f3f74f0cb8b7cd95452a1a7fb1a29d5f147b4967
quinoth <dima.rudak17@gmail.com>
2025-05-11 15:58:19 +0800

    Создание формы регистрации

---
+++ frontend/src/components/RegistrationForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const RegistrationForm = () => {
+    return (
+        <div>
+            <h2>Регистрация</h2>
+            <form>
+                <input type="text" placeholder="Имя пользователя" />
+            </form>
+        </div>
+    );
+};
+
+export default RegistrationForm;
