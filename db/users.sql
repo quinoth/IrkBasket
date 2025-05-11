1b4fd9887a696d94c1c1320feb7cb01813741448
quinoth <dima.rudak17@gmail.com>
2025-05-11 16:19:23 +0800

    Изменение юзернейма на Имя и Фамилию

--- db/users.sql
+++ db/users.sql
@@ -1,8 +1,9 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
