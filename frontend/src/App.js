456139bff144a8e6bcea41afcb16afe399e90e9d
quinoth <dima.rudak17@gmail.com>
2025-05-11 17:33:14 +0800

    Интеграция AuthProvider и маршрута /login в App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegistrationForm from './components/RegistrationForm';
+import LoginForm from './components/LoginForm';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/register" element={<RegistrationForm />} />
-            </Routes>
-        </Router>
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route path="/register" element={<RegistrationForm />} />
+                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="/" element={<div className="text-center mt-10">Главная страница</div>} />
+                </Routes>
+            </Router>
+        </AuthProvider>
     );
 }
 
