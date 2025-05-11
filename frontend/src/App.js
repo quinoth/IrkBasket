b9329f46d996aa5b94b6d9de66a9e2b69e43d801
quinoth <dima.rudak17@gmail.com>
2025-05-11 17:41:33 +0800

    Интеграция Navbar в App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegistrationForm from './components/RegistrationForm';
 import LoginForm from './components/LoginForm';
 import Profile from './components/Profile';
+import Navbar from './components/Navbar';
 import { AuthProvider } from './context/AuthContext';
 
 function App() {
     return (
         <AuthProvider>
             <Router>
+                <Navbar />
                 <Routes>
                     <Route path="/register" element={<RegistrationForm />} />
                     <Route path="/login" element={<LoginForm />} />
-					<Route path="/profile" element={<Profile />} />
+                    <Route path="/profile" element={<Profile />} />
                     <Route path="/" element={<div className="text-center mt-10">Главная страница</div>} />
                 </Routes>
             </Router>
